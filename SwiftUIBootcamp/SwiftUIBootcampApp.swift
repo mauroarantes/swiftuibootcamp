df041821ef42a8c0a12a42220dc6d6f3b5d2b62c
Mauro Arantes <mauro.arantes@gmail.com>
2022-02-15 13:27:55 +0000

    initial commit

---
+++ SwiftUIBootcamp/SwiftUIBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBootcampApp.swift
+//  SwiftUIBootcamp
+//
+//  Created by Mauro Arantes on 15/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
